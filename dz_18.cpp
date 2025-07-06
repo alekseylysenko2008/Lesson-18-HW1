67bfa9fb2fc84396a80b626632244c6708994003
alekseylysenko2008 <alexeytopka@gmail.com>
2025-07-06 12:23:04 +0300

    Add project files.

---
+++ dz_18.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Employee {
+    string surname;
+    int birthYear;
+    string position;
+    double salary;
+    string education;
+};
